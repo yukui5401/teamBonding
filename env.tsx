02da4191c9c16af6b24bc3d827cb2173f17bb407
samuel <leumasmontreal@hotmail.com>
2024-08-20 19:40:14 -0400

    Demenagement

---
+++ env.tsx
@@ -0,0 +1,3 @@
+export default keys = {
+  cohere: "ztE1JvmglRuQfvWQwXix6cEoYUKkNU4HBC1Q4Gt6",
+};
---
+++ frontend/env.tsx
@@ -0,0 +1,3 @@
+export default Keys = {
+  cohere: "ztE1JvmglRuQfvWQwXix6cEoYUKkNU4HBC1Q4Gt6",
+};
